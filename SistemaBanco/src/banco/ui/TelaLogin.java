468f0539ebd67c4ecbf3ce078f09eb40f6d01674
Keslley Antonio <keslleyjunior2008@gmail.com>
2026-06-14 22:09:37 +0000

    14 de jun. de 2026, 19:09

--- SistemaBanco/src/banco/ui/TelaLogin.java
+++ SistemaBanco/src/banco/ui/TelaLogin.java
@@ -9,7 +9,9 @@ public class TelaLogin extends javax.swing.JFrame {
     // GEN-BEGIN:initComponents
     private void initComponents() {
         btnEntrar = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        Teste = new javax.swing.JButton();
+        Textp = new javax.swing.JLabel();
+        Campo = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("My Frame");
@@ -22,19 +24,29 @@ public class TelaLogin extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("Button");
+        Teste.setText("ok");
+
+        Textp.setText("Texto do teste");
+
+        Campo.setText("oi");
 
         btnEntrar.setBounds(232, 264, 100, 30);
         getContentPane().add(btnEntrar);
-        jButton1.setBounds(152, 96, 100, 30);
-        getContentPane().add(jButton1);
+        Teste.setBounds(152, 96, 100, 30);
+        getContentPane().add(Teste);
+        Textp.setBounds(16, 96, 100, 30);
+        getContentPane().add(Textp);
+        Campo.setBounds(360, 96, 100, 30);
+        getContentPane().add(Campo);
         setSize(600, 450);
         setLocationRelativeTo(null);
     } // GEN-END:initComponents
 
     // GEN-BEGIN:variables
     private javax.swing.JButton btnEntrar;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton Teste;
+    private javax.swing.JLabel Textp;
+    private javax.swing.JTextField Campo;
     // GEN-END:variables
 
     private String usuario = "";
