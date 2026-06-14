e94744e5e88b9a3875f2821b12a9bdf903a566a4
Keslley Antonio <keslleyjunior2008@gmail.com>
2026-06-14 22:12:34 +0000

    14 de jun. de 2026, 19:12

--- SistemaBanco/src/banco/ui/TelaLogin.java
+++ SistemaBanco/src/banco/ui/TelaLogin.java
@@ -9,9 +9,11 @@ public class TelaLogin extends javax.swing.JFrame {
     // GEN-BEGIN:initComponents
     private void initComponents() {
         btnEntrar = new javax.swing.JButton();
-        Teste = new javax.swing.JButton();
+        botao = new javax.swing.JButton();
         Textp = new javax.swing.JLabel();
         Campo = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("My Frame");
@@ -24,29 +26,39 @@ public class TelaLogin extends javax.swing.JFrame {
             }
         });
 
-        Teste.setText("ok");
+        botao.setText("ok");
 
         Textp.setText("Texto do teste");
 
         Campo.setText("oi");
 
-        btnEntrar.setBounds(232, 264, 100, 30);
+        jPanel1.setLayout(null);
+
+        jButton1.setText("Button");
+
+        btnEntrar.setBounds(64, 184, 100, 30);
         getContentPane().add(btnEntrar);
-        Teste.setBounds(152, 96, 100, 30);
-        getContentPane().add(Teste);
+        botao.setBounds(152, 96, 100, 30);
+        getContentPane().add(botao);
         Textp.setBounds(16, 96, 100, 30);
         getContentPane().add(Textp);
         Campo.setBounds(360, 96, 100, 30);
         getContentPane().add(Campo);
+        jPanel1.setBounds(264, 184, 200, 150);
+        getContentPane().add(jPanel1);
+        jButton1.setBounds(32, 64, 100, 30);
+        jPanel1.add(jButton1);
         setSize(600, 450);
         setLocationRelativeTo(null);
     } // GEN-END:initComponents
 
     // GEN-BEGIN:variables
     private javax.swing.JButton btnEntrar;
-    private javax.swing.JButton Teste;
+    private javax.swing.JButton botao;
     private javax.swing.JLabel Textp;
     private javax.swing.JTextField Campo;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton jButton1;
     // GEN-END:variables
 
     private String usuario = "";
