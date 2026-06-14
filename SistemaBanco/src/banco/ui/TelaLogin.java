c92b78edbbe9320a23d417a0a7e17d8e20e4157b
Keslley Antonio <keslleyjunior2008@gmail.com>
2026-06-14 22:07:41 +0000

    14 de jun. de 2026, 19:07

--- SistemaBanco/src/banco/ui/TelaLogin.java
+++ SistemaBanco/src/banco/ui/TelaLogin.java
@@ -9,6 +9,7 @@ public class TelaLogin extends javax.swing.JFrame {
     // GEN-BEGIN:initComponents
     private void initComponents() {
         btnEntrar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("My Frame");
@@ -21,15 +22,22 @@ public class TelaLogin extends javax.swing.JFrame {
             }
         });
 
-        btnEntrar.setBounds(200, 248, 100, 30);
+        jButton1.setText("Button");
+
+        btnEntrar.setBounds(232, 264, 100, 30);
         getContentPane().add(btnEntrar);
+        jButton1.setBounds(152, 96, 100, 30);
+        getContentPane().add(jButton1);
         setSize(600, 450);
         setLocationRelativeTo(null);
     } // GEN-END:initComponents
 
     // GEN-BEGIN:variables
     private javax.swing.JButton btnEntrar;
+    private javax.swing.JButton jButton1;
     // GEN-END:variables
+
+    private String usuario = "";
 
     private void btnEntrarActionPerfomed(java.awt.event.ActionEvent evt) {
         // TODO add your handling code here:
