bb2595c7cfc40e45ad0592e23234d5c81ab9e592
Keslley Antonio <keslleyjunior2008@gmail.com>
2026-06-14 19:59:10 +0000

    14 de jun. de 2026, 16:59

---
+++ SistemaBanco/src/banco/ui/panel1/CustomButton1.java
@@ -0,0 +1,12 @@
+package banco.ui.panel1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CustomButton1 extends JButton {
+
+  public CustomButton1() {
+    super("Ok");
+    setBackground(new Color(225, 255, 77));
+  }
+}
