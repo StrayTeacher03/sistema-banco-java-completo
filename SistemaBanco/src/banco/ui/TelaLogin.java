1e06fe387a6f31a875d73b032da7ef36d64518d3
Keslley Antonio <keslleyjunior2008@gmail.com>
2026-06-15 22:05:51 +0000

    15 de jun. de 2026, 19:05

---
+++ SistemaBanco/src/banco/ui/TelaLogin.java
@@ -0,0 +1,22 @@
+package banco.ui;
+
+public class TelaLogin extends javax.swing.JFrame {
+
+    public TelaLogin() {
+        initComponents();
+    }
+
+    // GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("TelaLogin");
+        getContentPane().setLayout(null);
+
+        setSize(640, 480);
+        setLocationRelativeTo(null);
+    } // GEN-END:initComponents
+
+    // GEN-BEGIN:variables
+    // GEN-END:variables
+}
