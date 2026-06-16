1150a1e0d314c0c279a3bdf05c3a59c6fb937452
Keslley Antonio <keslleyjunior2008@gmail.com>
2026-06-16 02:33:04 +0000

    15 de jun. de 2026, 23:33

---
+++ SistemaBanco/src/banco/ui/TelaGerenciarUsuarios.java
@@ -0,0 +1,22 @@
+package banco.ui;
+
+public class TelaGerenciarUsuarios extends javax.swing.JFrame {
+
+    public TelaGerenciarUsuarios() {
+        initComponents();
+    }
+
+    // GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("TelaGerenciarUsuarios");
+        getContentPane().setLayout(null);
+
+        setSize(640, 480);
+        setLocationRelativeTo(null);
+    } // GEN-END:initComponents
+
+    // GEN-BEGIN:variables
+    // GEN-END:variables
+}
