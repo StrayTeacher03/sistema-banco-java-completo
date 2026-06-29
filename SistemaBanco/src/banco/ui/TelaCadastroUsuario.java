75a111156ebdd52a5b9c04cc2b55043413d5e47d
Keslley Antonio <keslleyjunior2008@gmail.com>
2026-06-29 03:19:43 +0000

    29 de jun. de 2026, 00:19

--- SistemaBanco/src/banco/ui/TelaCadastroUsuario.java
+++ SistemaBanco/src/banco/ui/TelaCadastroUsuario.java
@@ -8,21 +8,16 @@ public class TelaCadastroUsuario extends javax.swing.JFrame {
 
     // GEN-BEGIN:initComponents
     private void initComponents() {
-        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("TelaCadastroUsuario");
         getContentPane().setLayout(null);
 
-        jPanel1.setLayout(null);
-
-        jPanel1.setBounds(152, 120, 280, 224);
-        getContentPane().add(jPanel1);
-        setSize(640, 480);
+        setSize(480, 480);
         setLocationRelativeTo(null);
     } // GEN-END:initComponents
 
     // GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+
     // GEN-END:variables
 }
